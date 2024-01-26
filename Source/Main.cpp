7ab7ab780b50f9c4e96efcad5a9d0228ce032561
Michael Pettric <michaelepettric@gmail.com>
2024-01-26 12:41:46 -0800

    First commit

---
+++ Source/Main.cpp
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <Windows.h>
+
+void log(const char* format, ...) {
+	va_list arguments;
+	const int size = 1000;
+
+	va_start(arguments, format);
+
+	char myArray[size] = {};
+
+	vsnprintf(myArray, size, format, arguments);
+
+	va_end(arguments);
+
+	OutputDebugString(myArray);
+	OutputDebugString("\n");
+}
+
+LRESULT windowProcedure(HWND windowHandle, UINT messageType, WPARAM wParam, LPARAM lParam)
+{
+	LRESULT result = {};
+	switch (messageType) {
+	case WM_KEYDOWN: {
+		log("WM_KEYDOWN: %llu", wParam);
+		break;
+	}
+
+	default: {
+		result = DefWindowProc(windowHandle, messageType, wParam, lParam);
+	}
+	}
+
+	return result;
+}
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+	OutputDebugString("Hello World!\n");
+
+	WNDCLASS wndClass = {};
+	wndClass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wndClass.lpfnWndProc = windowProcedure;
+	wndClass.cbClsExtra = 0;
+	wndClass.cbWndExtra = 0;
+	wndClass.hInstance = hInstance;
+	wndClass.hIcon = NULL;
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass.hbrBackground = NULL;
+	wndClass.lpszMenuName = nullptr;
+	wndClass.lpszClassName = "MyClassName";
+
+	RegisterClass(&wndClass);
+
+	HWND window = CreateWindowEx(WS_EX_APPWINDOW, "MyClassName", "2D Project Chris", WS_OVERLAPPEDWINDOW, 
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL
+	);
+
+	ShowWindow(window, SW_SHOW);
+
+	MSG msg = {};
+
+	bool running = true;
+
+	while (running) {
+		while (PeekMessage(&msg, window, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+
+			if (msg.message == WM_QUIT) {
+				running = false;
+			}
+		}
+	}
+
+	return 0;
+}
